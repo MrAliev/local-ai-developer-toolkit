2966b487d9425ea085c70c75b6bb67d0eb93f3b1
Антон <46073415+MrAliev@users.noreply.github.com>
2026-08-07 15:54:10 +0300

    Prepare release 0.1.23 (#42)

--- scripts/publish-localai-release-win-x64-self-contained.ps1
+++ scripts/publish-localai-release-win-x64-self-contained.ps1
@@ -9,13 +9,13 @@ param(
     [string] $PublishRoot = "publish",
 
     [Parameter(Mandatory = $false)]
-    [string] $ReleaseVersion = "0.1.22",
+    [string] $ReleaseVersion = "0.1.23",
 
     [Parameter(Mandatory = $false)]
     [string] $VersionDirectory = "",
 
     [Parameter(Mandatory = $false)]
-    [string] $PackageUri = "https://github.com/MrAliev/local-ai-developer-toolkit/releases/download/0.1.22/localai-package.zip",
+    [string] $PackageUri = "https://github.com/MrAliev/local-ai-developer-toolkit/releases/download/0.1.23/localai-package.zip",
 
     [Parameter(Mandatory = $false)]
     [switch] $SignManifest
